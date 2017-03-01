5f69bcd4946b6c9df9d60077a8c7a9ae9bdf96cd
seanonthelawn <seanonthelawn@gmail.com>
2017-03-01 15:42:22 -0600

    Add files via upload

--- Pull.js
+++ Pull.js
@@ -14,5 +14,17 @@ $(document).ready(function(){
 
 	}).filter(':first').click();
 	//no Idea how any of this works
-
+	$('#button').click(function(){
+		distance = $('#distance').val();
+		mass1 = $('#mass1').val();
+		mass2 = $('#mass2').val();
+		
+		
+		
+			
+		$('#print').html( parseFloat(distance) + parseFloat(mass1) + parseFloat(mass2));
+		
+	});
+	
+	
 });
