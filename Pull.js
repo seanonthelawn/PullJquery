c3b8a7862200eb4bfa5ead43dfa3bbd03c33ba74
seanonthelawn <seanonthelawn@gmail.com>
2017-02-28 22:28:43 -0600

    Add files via upload

--- Pull.js
+++ Pull.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-   $('#container ul.nav a').click(function(e){
-	 
-		e.preventDefault();	 
-	 
-})
+
+	$('#container ul.tabs a').click(function(e){
+		
+		e.preventDefault();
+
+
+		$('#container > div').hide().filter(this.hash).show();
+
+		$('#container ul.tabs a').removeClass('selected');
+		
+
+		$(this).addClass('selected');
+
+	}).filter(':first').click();
+	//no Idea how any of this works
+
+});
