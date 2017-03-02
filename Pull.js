91a05c75634290e386187db75e09ce835c1fd80e
seanonthelawn <seanonthelawn@gmail.com>
2017-03-02 10:31:19 -0600

    Add files via upload

--- Pull.js
+++ Pull.js
@@ -6,23 +6,30 @@ $(document).ready(function(){
 
 
 		$('#container > div').hide().filter(this.hash).show();
+        //
 
 		$('#container ul.tabs a').removeClass('selected');
-		
+		//This clears the "selected" class from all of the links on click.
 
 		$(this).addClass('selected');
-
+        // this adds a "selected" class to the current tab, allowing me to edit CSS rules on the selected tab only.
+        
+        
 	}).filter(':first').click();
-	//no Idea how any of this works
+	//This line programatically clicks the first tab on load.
+//This is from an in class example    
+    
+    
 	$('#button').click(function(){
-		distance = $('#distance').val();
-		mass1 = $('#mass1').val();
-		mass2 = $('#mass2').val();
+		distance = parseFloat($('#distance').val());
+		mass1 = parseFloat($('#mass1').val());
+		mass2 = parseFloat($('#mass2').val());
+        //This is where I pull all my data
 		
 		
 		
 			
-		$('#print').html( (0.00000000006673 * parseFloat(mass1) * parseFloat(mass2))/(parseFloat(distance)*parseFloat(distance)) + " N");
+		$('#print').html((0.00000000006673 * mass1 * mass2/(distance * distance) + " N"));
 		
 	});
 	
